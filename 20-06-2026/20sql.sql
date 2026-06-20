97f69343753f31ae4be63cc1c471b2fffef5a471
pranav260605 <pgnvspranav@gmail.com>
2026-06-20 22:56:20 +0530

    sql

---
+++ 20-06-2026/20sql.sql
@@ -0,0 +1,3 @@
+SELECT user_id,tweet_date,ROUND(avg(tweet_count)over(partition by user_id order by tweet_date ROWS BETWEEN 2 PRECEDING AND CURRENT ROW
+),2)as rolling_avg                                             
+FROM tweets;
