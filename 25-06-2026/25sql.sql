8dd22d4a9e5b676f2328967ee867b609881c185e
pranav260605 <pgnvspranav@gmail.com>
2026-06-25 23:15:17 +0530

    sql

---
+++ 25-06-2026/25sql.sql
@@ -0,0 +1,7 @@
+SELECT 
+  manufacturer, 
+  CONCAT( '$', ROUND(SUM(total_sales) / 1000000), ' million') AS sales_mil 
+FROM pharmacy_sales 
+GROUP BY manufacturer 
+ORDER BY SUM(total_sales) DESC, manufacturer;
+
